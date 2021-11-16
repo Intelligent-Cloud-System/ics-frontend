022d326bc77d7a5881998fa08fb56ff53fb54c59
Honchar007 <54860596+Honchar007@users.noreply.github.com>
2021-11-16 22:09:33 +0200

    implement login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+// components
+import App from './App';
+
+// styles
 import './index.css';
 
 ReactDOM.render(
-	<React.StrictMode>Intelligent Cloud System</React.StrictMode>,
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
 	document.getElementById('root'),
 );
