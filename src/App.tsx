f9ea037beb6f6cf85fd0a5627fb490a03fc513b4
Honchar007 <54860596+Honchar007@users.noreply.github.com>
2021-11-22 17:15:21 +0200

    implement register page and validation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 // components
 import Home from 'pages/Home';
 import Login from 'pages/Login';
+import Registration from 'pages/Registration';
 
 function App() {
 	return (
@@ -15,6 +16,9 @@ function App() {
 					</Route>
 					<Route path='/login'>
 						<Login />
+					</Route>
+					<Route path='/register'>
+						<Registration />
 					</Route>
 					<Route path='*'>
 						<Redirect to='/home' />
