b633027c62ac29e17faf2d8a1770379c7c130043
tedi4t <tedi4t@gmail.com>
2021-12-05 22:05:45 +0200

    query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 
 import { AppRoutes } from 'App.routes';
@@ -8,14 +9,16 @@ import { SnackbarProvider } from 'hooks/notification/snackbar.provider';
 function App() {
 	useAutoTokenRefresh();
 
+	const queryClient = new QueryClient();
+
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<StyledEngineProvider injectFirst>
 				<SnackbarProvider>
 					<AppRoutes />
 				</SnackbarProvider>
 			</StyledEngineProvider>
-		</>
+		</QueryClientProvider>
 	);
 }
 
