445310c1ac062a909b571d2be3d191ec76dc6ec5
Uryn Dmytro <dmytro@digia.co>
2022-05-24 15:20:37 +0300

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { ThemeProvider as MuiThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 
 import { AppRoutes } from 'App.routes';
 import { useAutoTokenRefresh } from 'hooks/auth/useAuthTokenRefresh';
 import { SnackbarProvider } from 'hooks/notification/snackbar.provider';
 import { OpenAPI as CoreOpenAPi } from 'clients/CoreService';
 import { UserContextProvider } from 'context/UserContext';
+import theme from 'themes';
 
 CoreOpenAPi.BASE = process.env.REACT_APP_CORE_URL as string;
 
@@ -17,13 +19,17 @@ function App() {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<StyledEngineProvider injectFirst>
-				<SnackbarProvider>
-					<UserContextProvider>
-						<AppRoutes />
-					</UserContextProvider>
-				</SnackbarProvider>
-			</StyledEngineProvider>
+			<StyledThemeProvider theme={theme}>
+				<StyledEngineProvider injectFirst>
+					<MuiThemeProvider theme={theme}>
+						<SnackbarProvider>
+							<UserContextProvider>
+								<AppRoutes />
+							</UserContextProvider>
+						</SnackbarProvider>
+					</MuiThemeProvider>
+				</StyledEngineProvider>
+			</StyledThemeProvider>
 		</QueryClientProvider>
 	);
 }
