1cd383e091a18b48c231b65bed19e8529b6f583c
MaksGovor <maksgovruha@gmail.com>
2022-06-23 12:19:27 +0300

    Fix: fixed creating bg colour for avatars

--- src/shared/string-avatar.ts
+++ src/shared/string-avatar.ts
@@ -1,21 +1,7 @@
-function stringToColor(string: string) {
-	let hash = 0;
-	let i;
+import crypto from 'crypto';
 
-	/* eslint-disable no-bitwise */
-	for (i = 0; i < string.length; i += 1) {
-		hash = string.charCodeAt(i) + ((hash << 5) - hash);
-	}
-
-	let color = '#';
-
-	for (i = 0; i < 3; i += 1) {
-		const value = (hash >> (i * 8)) & 0xff;
-		color += `00${value.toString(16)}`.substr(-2);
-	}
-	/* eslint-enable no-bitwise */
-
-	return color;
+function stringToColor(str: string) {
+	return '#' + crypto.createHash('SHA256').update(str).digest('hex').slice(0, 6);
 }
 
 export function stringAvatar(name: string) {
